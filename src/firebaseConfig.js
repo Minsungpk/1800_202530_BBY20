6affb54cdff758a2b0fec63eefce39c5efd6aafe
Jessie Yuen <jyuen55@my.bcit.ca>
2025-11-29 15:57:59 -0800

    added pages for friends requesting

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,6 @@
-// src/firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth }       from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDD_2z29qDHPVXeSXyZ0T9VO_n_PcW1EqU",
@@ -10,10 +9,10 @@ const firebaseConfig = {
   storageBucket: "group-project-8a6ee.firebasestorage.app",
   messagingSenderId: "922685674876",
   appId: "1:922685674876:web:6edeac0ff4fb485db780f9",
-  measurementId: "G-1LCVJ62HEL"
+  measurementId: "G-1LCVJ62HEL",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db   = getFirestore(app);
+export const db = getFirestore(app);
