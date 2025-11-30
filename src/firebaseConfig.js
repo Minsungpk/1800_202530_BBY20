49614a5c7ae5157d020971ab0d17f23eed51ac02
Jessie Yuen <jyuen55@my.bcit.ca>
2025-11-29 16:31:04 -0800

    changed pages

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,3 +1,4 @@
+// firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
@@ -12,7 +13,6 @@ const firebaseConfig = {
   measurementId: "G-1LCVJ62HEL",
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
